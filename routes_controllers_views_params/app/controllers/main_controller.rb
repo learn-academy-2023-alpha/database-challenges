3859832da737b7cbcbb81a8271a92e916c052811
Aaron Criner <aaron.criner@me.com>
2023-03-28 11:57:46 -0700

    Completed Initial Challenges

---
+++ routes_controllers_views_params/app/controllers/main_controller.rb
@@ -0,0 +1,12 @@
+class MainController < ApplicationController
+    # def lander
+    #     render html: 'Welcome to our working site'
+    # end
+    
+    def lander
+    end
+    def aaron
+    end
+    def yahya
+    end
+end
